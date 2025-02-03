278053a45de0f7f91e0723bfbcdf166a2d19f5ea
APIVerve Team <hello@apiverve.com>
2025-02-03 15:49:08 -0600

    Minor Version Update

---
+++ NobelPrizesQueryOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class NobelPrizesQueryOptions {
+public string firstname { get; set; }
+public string lastname { get; set; }
+public string category { get; set; }
+public string year { get; set; }
+}
+
+}
